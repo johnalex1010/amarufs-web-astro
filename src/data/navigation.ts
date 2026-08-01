81ee3bc299ba3153ef2a2fe97bfff5ca77caf5f5
John Alexander Fandiño <johnalexfan@gmail.com>
2026-08-01 17:29:30 -0500

    fix: Mejora del Menú

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,40 +1,40 @@
 export const navigation = [
-  {
-    label: "Inicio",
-    href: "/",
-    icon: "fa-solid fa-house",
-    key: "inicio"
-  },
-  {
-    label: "Propietarios",
-    href: "/propietarios/",
-    icon: "fa-solid fa-key",
-    key: "propietarios",
-    children: [
-      {
-        label: "Propietarios - Venta",
-        href: "/propietarios/venta/",
-        icon: "fa-solid fa-sign-hanging",
-        key: "propietarios-venta"
-      },
-      {
-        label: "Propietarios - Arriendo",
-        href: "/propietarios/arriendo/",
-        icon: "fa-solid fa-door-open",
-        key: "propietarios-arriendo"
-      }
-    ]
-  },
-  {
-    label: "Arrendatarios",
-    href: "/arrendatarios/",
-    icon: "fa-solid fa-door-open",
-    key: "arrendatarios"
-  },
-  {
-    label: "Referidos",
-    href: "/referidos/",
-    icon: "fa-solid fa-handshake-angle",
-    key: "referidos"
-  }
+	{
+		label: "Inicio",
+		href: "/",
+		icon: "fa-solid fa-house",
+		key: "inicio",
+	},
+	{
+		label: "Propietarios",
+		href: "/propietarios/",
+		icon: "fa-solid fa-key",
+		key: "propietarios",
+		children: [
+			{
+				label: "Venta",
+				href: "/propietarios/venta/",
+				icon: "fa-solid fa-sign-hanging",
+				key: "propietarios-venta",
+			},
+			{
+				label: "Arriendo",
+				href: "/propietarios/arriendo/",
+				icon: "fa-solid fa-door-open",
+				key: "propietarios-arriendo",
+			},
+		],
+	},
+	{
+		label: "Arrendatarios",
+		href: "/arrendatarios/",
+		icon: "fa-solid fa-door-open",
+		key: "arrendatarios",
+	},
+	{
+		label: "Referidos",
+		href: "/referidos/",
+		icon: "fa-solid fa-handshake-angle",
+		key: "referidos",
+	},
 ];
